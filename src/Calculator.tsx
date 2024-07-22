64529868fcb61b50c268ae97a27647157173f00d
Chris Cooley <ccooley@bonadmin.com>
2024-07-22 16:17:29 -0600

    Add Dog Age form

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
+  const [dogAge, setDogAge] = useState("");
+
   return (
-    <></>
+    <>
+      <form>
+        <div>
+          <label htmlFor="dogAge">Dog Age</label>
+          <input
+            type="number"
+            id="dogAge"
+            value={dogAge}
+            placeholder="Enter dog age in years"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+              setDogAge(event.target.value)}
+          />
+        </div>
+      </form>
+    </>
   )
 }
-
 export default Calculator;
